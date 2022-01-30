17a63dd0db02dd8b6ac25cff5180d5d2cd0bdd24
Chaikal Amrullah <chaikal.amrullah@gmail.com>
2022-01-30 04:27:43 +0100

    adding new sider

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -19,9 +19,11 @@ const AboutPage = () => {
           The application is made to find out user preferences related to symbol design for navigation applications on Augmented Reality (AR) devices.
           Symbols that can be easily understood by users are expected to increase user comfort in using the device and can make it easier for users to remember travel routes.
 
-          Before you start, make sure you watch the app usage guide. The application user guide video can be seen at the following link...
+          Before you start, make sure you watch the app usage guide.
+          The application user guide video can be seen at the following link...
 
-          The link to download the Image and supporting information can be found at the following link... .
+          The link to download the Image and supporting information can be found
+          at the following link... .
 
           The source of the icon used in this application comes from the following link:
 
