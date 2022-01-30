71b659becb5bd7d328c62ea1daf448614d3750bf
Chaikal Amrullah <chaikal.amrullah@gmail.com>
2022-01-30 04:27:17 +0100

    adding new sider

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -15,7 +15,9 @@ const AboutPage = () => {
       <p> Hey there!
           First of all thank you for participating in the trial of this app.
 
-          This application was made as part of the Project Cartography coursework (Winter Semester 2021/2022). The application is made to find out user preferences related to symbol design for navigation applications on Augmented Reality (AR) devices. Symbols that can be easily understood by users are expected to increase user comfort in using the device and can make it easier for users to remember travel routes.
+          This application was made as part of the Project Cartography coursework (Winter Semester 2021/2022) with title "Building a Design-It-Yourself Navigation Web App Interface".
+          The application is made to find out user preferences related to symbol design for navigation applications on Augmented Reality (AR) devices.
+          Symbols that can be easily understood by users are expected to increase user comfort in using the device and can make it easier for users to remember travel routes.
 
           Before you start, make sure you watch the app usage guide. The application user guide video can be seen at the following link...
 
