07f04aa9b55e2defe6df9da89a5e800e7aa4a57f
Chaikal Amrullah <chaikal.amrullah@gmail.com>
2022-01-30 05:55:50 +0100

    editing about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -12,23 +12,22 @@ const AboutPage = () => {
       }}
     >
       <h2>About</h2>
-      <p> Hey there!\n
-          First of all thank you for participating in the trial of this app.\n
+      <p> Hey there!
+          <br>First of all thank you for participating in the trial of this app.
+          This application was made as part of the Project Cartography coursework (Winter Semester 2021/2022)
+          with title "Building a Design-It-Yourself Navigation Web App Interface".
+          The application is made to find out user preferences related to
+          symbol design for navigation applications on Augmented Reality (AR) devices.
+          Symbols that can be easily understood by users are expected
+          to increase user comfort in using the device and can make it easier
+          for users to remember travel routes.
 
-          This application was made as part of the Project Cartography coursework (Winter Semester 2021/2022)\n
-          with title "Building a Design-It-Yourself Navigation Web App Interface".\n
-          The application is made to find out user preferences related to\n
-          symbol design for navigation applications on Augmented Reality (AR) devices.\n
-          Symbols that can be easily understood by users are expected\n
-          to increase user comfort in using the device and can make it easier\n
-          for users to remember travel routes.\n
-          \n
-          Before you start, make sure you watch the app usage guide.\n
-          The application user guide video can be seen at the following link... .\n
-          \n
-          The link to download the Image and supporting information can be found\n
-          at the following link... .\n
-          \n
+          Before you start, make sure you watch the app usage guide.
+          The application user guide video can be seen at the following link... .
+
+          The link to download the Image and supporting information can be found
+          at the following link... .
+
           The source of the icon used in this application comes from the following link:
 
           - Floor plans
